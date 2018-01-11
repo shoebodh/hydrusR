0af590059c291c180eac2bfbf5f86315c311df84
Subodh <shoebodh@gmail.com>
2018-01-11 16:29:11 -0500

    updates

--- R/example_h1d.R
+++ R/example_h1d.R
@@ -1,15 +1,11 @@
 #' Runs a test hydrus project on the system
 #'
-#' @param
+#' @param ...
 #'
 #' @export
 #'
-#' @example
-#'
-#' example.h1d()
 
-
-example.h1d<- function(){
+example.h1d<- function(...){
       example_h1d = system.file("examples/example.R", package = "hydrusR")
    source(example_h1d)
 }
